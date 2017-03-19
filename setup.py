e4129486f8fc1449f2e936187991edbc6cf4ade0
Sebastian Rittau <srittau@rittau.biz>
2017-03-19 23:33:40 +0100

    add more meta information

--- setup.py
+++ setup.py
@@ -1,16 +1,29 @@
 #!/usr/bin/python3
 
+import os.path
 from setuptools import setup
+
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 
 setup(
     name="json-get",
     version="1.0",
     description="Get values from JSON objects using a path expression",
+    long_description=read("README.rst"),
     author="Sebastian Rittau",
     author_email="srittau@rittau.biz",
     url="https://github.com/srittau/python-json-get",
     py_modules=["jsonget", "jsonget_test"],
     tests_require=["asserts >= 0.6"],
     license="MIT",
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+    ]
 )
