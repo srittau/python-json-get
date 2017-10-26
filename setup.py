84b2e2cfc4ca93a7f5263d99fc8277d6b5a5cfba
Sebastian Rittau <srittau@rittau.biz>
2017-10-27 00:19:39 +0200

    [setup] add Trove classifiers

--- setup.py
+++ setup.py
@@ -21,9 +21,11 @@ setup(
     license="MIT",
     classifiers=[
         "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
         "License :: OSI Approved :: MIT License",
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
+        "Topic :: Software Development",
     ]
 )
